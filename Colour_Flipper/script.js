a43156df118762cf95b4e991c7ea59d6b09eecd5
Dashen Dhanabal <dashendhanabal94@gmail.com>
2022-02-23 21:01:59 +0800

    Created hex.js & made small edits to index.html

--- Colour_Flipper/script.js
+++ Colour_Flipper/script.js
@@ -6,11 +6,11 @@ btn.addEventListener('click', function () {
     //Get random number between 0 and 3
     const randomNumber = getRandomNumber();
     console.log(randomNumber);
-    document.body.style.backgroundColor = colours[randomNumber];
+    document.body.style.backgroundColor = colours[getRandomNumber()];
     colour.textContent = colours[randomNumber];
 });
 
 function getRandomNumber() {
 
-    return Math.floor(Math.random() * colours.length)
+    return Math.floor(Math.random() * colours.length)//Math.floor will give round down numbers
 };
